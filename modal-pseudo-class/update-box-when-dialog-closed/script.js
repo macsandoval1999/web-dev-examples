12df2f29274b5541e9c3352deec30b5d57f60b21
macsandoval1999 <msandoval2@byupathway.edu>
2025-11-20 20:47:22 -0700

    Modals examples added

---
+++ modal-pseudo-class/update-box-when-dialog-closed/script.js
@@ -0,0 +1,29 @@
+const updateButton = document.getElementById("updateDetails");
+const favDialog = document.getElementById("favDialog");
+const outputBox = document.querySelector("output");
+const selectEl = favDialog.querySelector("select");
+const confirmBtn = favDialog.querySelector("#confirmBtn");
+
+// If a browser doesn't support the dialog, then hide the
+// dialog contents by default.
+if (typeof favDialog.showModal !== "function") {
+    favDialog.hidden = true;
+    // Your fallback script
+}
+// "Update details" button opens the <dialog> modally
+updateButton.addEventListener("click", () => {
+    if (typeof favDialog.showModal === "function") {
+        favDialog.showModal();
+    } else {
+        outputBox.value = "Sorry, the dialog API is not supported by this browser.";
+    }
+});
+// "Favorite animal" input sets the value of the submit button
+selectEl.addEventListener("change", (e) => {
+    confirmBtn.value = selectEl.value;
+});
+// "Confirm" button of form triggers "close" on dialog because of [method="dialog"]
+favDialog.addEventListener("close", () => {
+    outputBox.value = `${favDialog.returnValue
+        } button clicked - ${new Date().toString()}`;
+});
